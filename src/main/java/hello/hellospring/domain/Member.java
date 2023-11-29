5a7134586c471be75278aa08d28d691ee2f032f1
kmc77 <k_minchoi@naver.com>
2023-11-29 18:31:40 +0900

    MemoryMemberRepositoryTest 코드 add..

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -17,6 +17,8 @@ public class Member {
         return name;
     }
 
-    public void setName(String spring) {
+    public void setName(String name) {
+        this.name = name;
     }
+
 }
--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -13,7 +13,7 @@ public class MemoryMemberRepositoryTest {
     public void save() {
         Member member = new Member();
         member.setName("spring");
-
+ 
         repository.save(member);
 
         Member result = repository.findById(member.getId()).get();
