a6e84198051e509b8d6085c3081523d707ef169e
kmc77 <k_minchoi@naver.com>
2023-12-07 18:50:00 +0900

    JDBC 템플릿 버젼 디비연동 완료

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -23,19 +23,27 @@ public class MemberService {
     */
 
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원을 검증합니다.
-        validateDuplicateMember(member);
 
-        // 회원을 저장하고, 고유 식별자를 반환합니다.
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
+
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName()).ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
     }
+
 
     /*
     전체 회원 조회
